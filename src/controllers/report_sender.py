5a2cd2205581f1b98ea037113ce98de9902ef834
vas g <vas.g@me.com>
2024-05-20 17:27:19 +0400

    working on report sender

--- src/controllers/report_sender.py
+++ src/controllers/report_sender.py
@@ -1,26 +1,46 @@
+import asyncio
 import logging
 
 import aiohttp
 
+from config import settings
+from controllers.crypt import Crypt
+from enums import Task
+
 logger = logging.getLogger(__name__)
 
 
-async def send_report(nickname: str, amount: int) -> None:
+async def send_report(task: Task, status: int = 1) -> None:
     async with aiohttp.ClientSession() as session:
-        url = ''
+        cryptor = Crypt(settings.key_encrypt, settings.key_decrypt)
+        url = settings.REPORT_ENDPOINT
+        data_json = task.model_dump_json()
         data = {
-            "order_id": 123,
-            "user_id": 123,
-            "requisite": "pokermaster",
-            "amount": 5000.00,
-            "status": 0,  # 0 - Not payed | 1 - Payed
+            'order_id': task.order_id,
+            'user_id': task.user_id,
+            'requisite': task.requisite,
+            'amount': task.amount,
+            'status': task.status
         }
-        text_ok = f'Transfer to: {nickname}, amount: ${amount}, status: SUCCESS'
-        text_error = f'Transfer to: {nickname}, amount: ${amount}, status: FAILED: ' + '{}. {}'
+        headers = {'x-simpleex-sign': cryptor.encrypt(data_json)}
+        suffix = 'SUCCESS' if status == 1 else 'FAIL'
+        text_ok = f'report sent: {task.order_id}|{task.user_id}|{task.requisite}|${task.amount}|{suffix}'
+        text_not_ok = f'report sent: {task.order_id}|{task.user_id}|{task.requisite}|${task.amount}|{suffix} with response: ' + '{}. {}'
 
-    async with session.post(url, json=data) as response:
-        if response.status == 200:
-            logger.info(text_ok)
-        else:
-            error_text = await response.text()
-            logger.info(text_error.format(response.status, error_text))
+        async with session.post(url, data=data, headers=headers) as response:
+            if response.status == 200:
+                logger.info(text_ok)
+                print(await response.json())
+            else:
+                error_text = await response.text()
+                logger.info(text_not_ok.format(response.status, error_text))
+                print(await response.json())
+    print(data)
+
+
+def run_main():
+    asyncio.run(send_report(task=Task(order_id=1, user_id=1, requisite='Mein Herz Brent', amount=1.00, status=1)))
+
+
+if __name__ == '__main__':
+    run_main()
