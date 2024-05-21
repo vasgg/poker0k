5231fd223585eea13793280e43a14a0b8c8d000f
vas g <vas.g@me.com>
2024-05-21 14:44:48 +0400

    working on send report

--- src/controllers/report_sender.py
+++ src/controllers/report_sender.py
@@ -10,7 +10,7 @@ from enums import Task
 logger = logging.getLogger(__name__)
 
 
-async def send_report(task: Task, status: int = 1) -> None:
+async def send_report(task: Task) -> None:
     async with aiohttp.ClientSession() as session:
         cryptor = Crypt(settings.key_encrypt, settings.key_decrypt)
         url = settings.REPORT_ENDPOINT
@@ -23,9 +23,8 @@ async def send_report(task: Task, status: int = 1) -> None:
             'status': task.status
         }
         headers = {'x-simpleex-sign': cryptor.encrypt(data_json)}
-        suffix = 'SUCCESS' if status == 1 else 'FAIL'
-        text_ok = f'report sent: {task.order_id}|{task.user_id}|{task.requisite}|${task.amount}|{suffix}'
-        text_not_ok = f'report sent: {task.order_id}|{task.user_id}|{task.requisite}|${task.amount}|{suffix} with response: ' + '{}. {}'
+        text_ok = f'report sent: {task.order_id}|{task.user_id}|{task.requisite}|${task.amount}|{task.status}'
+        text_not_ok = f'report sent: {task.order_id}|{task.user_id}|{task.requisite}|${task.amount}|{task.status} with response: ' + '{}. {}'
 
         async with session.post(url, data=data, headers=headers) as response:
             if response.status == 200:
