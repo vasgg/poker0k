1710821231c58cbb5ca2cc6e0ebc61a635f39db2
vas g <vas.g@me.com>
2024-06-26 15:58:02 +0400

    wip

--- src/utilites.py
+++ src/utilites.py
@@ -4,7 +4,7 @@ import psutil
 def print_non_system_processes():
     for proc in psutil.process_iter(['pid', 'name', 'username']):
         try:
-            if proc.info['username'] not in ['SYSTEM', 'root']:
+            if proc.info['name'] in ['GGnet.exe', 'pokerok.exe']:
                 print(f"PID: {proc.info['pid']}, User: {proc.info['username']}, Name: {proc.info['name']}")
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
