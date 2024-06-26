dc091e40ef3bb23177c8383a61baedc4d2ae9e14
vas g <vas.g@me.com>
2024-06-26 16:14:28 +0400

    wip

--- src/utilites.py
+++ src/utilites.py
@@ -7,10 +7,12 @@ def check_ggnet_app():
     for proc in psutil.process_iter(['pid', 'name', 'username']):
         try:
             if proc.info['name'] in ['GGnet.exe']:
+                print('GGnet is running')
                 return
             else:
                 application_path = r"C:\Users\Administrator\AppData\Roaming\POKEROK\bin\Loader\GGnet.exe"
                 os.startfile(application_path)
+                print('Starting GGnet')
                 return
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
