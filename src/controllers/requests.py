3576144f15716a568aa2b361b666a9f19d9ba55a
vasgg <=>
2024-08-02 17:07:46 +0400

    reworked report with retry

--- src/controllers/requests.py
+++ src/controllers/requests.py
@@ -11,7 +11,7 @@ from internal import Task
 logger = logging.getLogger(__name__)
 
 
-async def send_report(task: Task, problem: str | None = None) -> None:
+async def send_report(task: Task, problem: str | None = None, retries: int = 3, delay: int = 3) -> None:
     async with aiohttp.ClientSession() as session:
         cryptor = Crypt(settings.key_encrypt, settings.key_decrypt)
         data_json = task.model_dump_json()
@@ -26,19 +26,23 @@ async def send_report(task: Task, problem: str | None = None) -> None:
         }
         headers = {'x-simpleex-sign': cryptor.encrypt(data_json)}
         text_ok = f'report sent: {task.order_id}|{task.user_id}|{task.requisite}|${task.amount}|{task.status}'
-        text_not_ok = (
-            f'report sent: {task.order_id}|{task.user_id}|{task.requisite}|${task.amount}|{task.status} '
-            f'with response: '
-        ) + '{}. {}'
+        text_not_ok = f'report sent: {task.order_id}|{task.user_id}|{task.requisite}|${task.amount}|{task.status} with response:'
 
-        async with session.post(task.callback_url, data=data, headers=headers) as response:
-            if response.status == HTTPStatus.OK:
-                logger.info(text_ok)
-                # print(await response.json())
-            else:
-                error_text = await response.text()
-                logger.info(text_not_ok.format(response.status, error_text))
-                # print(await response.json())
+        for attempt in range(retries):
+            try:
+                async with session.post(task.callback_url, data=data, headers=headers) as response:
+                    if response.status == HTTPStatus.OK:
+                        logger.info(text_ok)
+                        return
+                    else:
+                        error_text = await response.text()
+                        logger.info(f"{text_not_ok} {response.status}. {error_text}")
+            except Exception as e:
+                logger.error(f"Attempt {attempt + 1} failed with error: {e}")
+
+            if attempt < retries - 1:
+                await asyncio.sleep(delay)
+        logger.error(f"Failed to send report after {attempt} attempts, task id: {task.order_id} failed with error: {e}.")
 
 
 async def send_queue_request() -> None:
