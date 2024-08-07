407184b32d2c5f8730e78b16b81a7a4454dc04a4
vasgg <=>
2024-08-08 01:25:42 +0400

    fix redis

--- src/reporter.py
+++ src/reporter.py
@@ -2,7 +2,7 @@ import asyncio
 from datetime import datetime
 import logging.config
 
-import redis
+import redis.asyncio as redis
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import DateTime, String, func
