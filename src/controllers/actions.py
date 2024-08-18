14a535cd8c3f5d0b012a3bdc6a22a36b104d235f
vasgg <=>
2024-08-18 20:10:37 +0400

    wip

--- src/controllers/actions.py
+++ src/controllers/actions.py
@@ -20,13 +20,13 @@ class Actions:
     @staticmethod
     async def click_on_const(mouse: Controller, coords: Coords, delay_after: int = 0, delay_before: int = 0):
         if delay_before > 0:
-            logger.info(f"Waiting {delay_before} seconds before clicking {coords.name.replace('_', ' ')}...")
+            # logger.info(f"Waiting {delay_before} seconds before clicking {coords.name.replace('_', ' ')}...")
             await asyncio.sleep(delay_before)
         mouse.position = coords.value
         mouse.click(Button.left)
-        logger.info(f"Mouse clicked: {coords.name}...")
+        logger.info(f"Mouse clicked: {coords.name.replace('_', ' ')}...")
         if delay_after > 0:
-            logger.info(f"Waiting {delay_after} seconds after clicking {coords.name.replace('_', ' ')}...")
+            # logger.info(f"Waiting {delay_after} seconds after clicking {coords.name.replace('_', ' ')}...")
             await asyncio.sleep(delay_after)
 
     @staticmethod
