ee5bef83ad3a8ab790017d04f795c9c5204242d9
vas g <vas.g@me.com>
2024-09-29 03:14:57 +0400

    wip

--- src/controllers/actions.py
+++ src/controllers/actions.py
@@ -130,7 +130,7 @@ class Actions:
             return
         await Actions.click_on_const(mouse, Coords.ANDROID_OPEN_EMULATOR_BUTTON)
         await Actions.click_on_const(mouse, Coords.ANDROID_OPEN_EMULATOR_BUTTON, 60)
-        if not WindowChecker.check_window():
+        if not await WindowChecker.check_window():
             await Actions.reopen_emulator(mouse, size=Size.BIG, attempts=attempts + 1)
             return
         await Actions.click_on_const(mouse, Coords.ANDROID_DONT_SHOW_TODAY, 5)
