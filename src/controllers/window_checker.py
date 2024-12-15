6ee173e6a3d00c005ed56fcdd9beab0bf82ebad6
vas g <vas.g@me.com>
2024-12-15 14:06:00 +0400

    work with window

--- src/controllers/window_checker.py
+++ src/controllers/window_checker.py
@@ -9,12 +9,13 @@ class WindowChecker:
     async def check_window() -> bool:
         windows = gw.getAllWindows()
         for window in windows:
-            if window.title == 'BlueStacks App Player':
-                if window.width == 428 and window.left == 1067:
-                    logger.info("Checking BlueStacks Player. Size and position: GOOD.")
-                    return True
-        logger.info(
-            "Checking BlueStacks Player. Size and position: BAD.\n"
-            "                                          Going to restart emulator."
-        )
-        return False
+            print(window.title, window.width, window.left)
+        #     if window.title == 'BlueStacks Player':
+        #         if window.width == 428 and window.left == 1067:
+        #             logger.info("Checking BlueStacks Player. Size and position: GOOD.")
+        #             return True
+        # logger.info(
+        #     "Checking BlueStacks Player. Size and position: BAD.\n"
+        #     "                                          Going to restart emulator."
+        # )
+        # return False
