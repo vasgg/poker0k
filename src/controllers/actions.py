655c640c41780912dd837d53ac8c77da538be14c
vas g <vas.g@me.com>
2025-01-08 16:04:50 +0400

    wip window check

--- src/controllers/actions.py
+++ src/controllers/actions.py
@@ -190,9 +190,10 @@ class Actions:
 async def start_app_flow(mouse: Controller, attempts: int = 1):
     await Actions.click_on_const(mouse, Coords.OPEN_APP_BUTTON)
     await Actions.click_on_const(mouse, Coords.OPEN_APP_BUTTON, 35)
-    if not await WindowChecker.check_window():
-        await Actions.reopen_pokerok(mouse, attempts=attempts + 1)
-        return
+    await WindowChecker.check_window()
+    # if not await WindowChecker.check_window():
+    #     await Actions.reopen_pokerok(mouse, attempts=attempts + 1)
+    #     return
     await Actions.click_on_const(mouse, Coords.LOGIN_BUTTON, 10)
     await Actions.click_on_const(mouse, Coords.CONFIRM_LOGIN_BUTTON, 15)
     await Actions.click_on_const(mouse, Coords.CLOSE_BANNER_BUTTON, 10)
