d85f0c0d7125208b2535a1a6cbe511bb9c457577
vas g <vas.g@me.com>
2025-03-08 01:51:38 +0400

    working on reports

--- src/controllers/telegram.py
+++ src/controllers/telegram.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 
 import aiofiles
-import aiohttp
-from aiohttp import FormData
+from aiohttp import FormData, ClientSession
 from requests import get
 
 from config import settings
@@ -11,31 +10,33 @@ from internal import Task
 
 async def send_telegram_report(message: str, task: Task | None = None, image_path: Path | None = None) -> None:
     text = f"{task.order_id}|{task.requisite}|${task.amount}|{message}" if task else message
-    if image_path:
-        url = f"https://api.telegram.org/bot{settings.TG_BOT_TOKEN.get_secret_value()}/sendPhoto"
-        data = FormData()
-        data.add_field("chat_id", f"{settings.TG_ID}")
-        data.add_field("caption", text)
-        data.add_field("disable_notification", "false")
-        async with aiofiles.open(image_path, "rb") as photo:
-            photo_data = await photo.read()
-            data.add_field("photo", photo_data, filename=image_path.name)
+    for chat_id in settings.REPORT_TG_IDS:
+        if image_path:
+            url = f"https://api.telegram.org/bot{settings.TG_BOT_TOKEN.get_secret_value()}/sendPhoto"
+            data = FormData()
+            data.add_field("chat_id", f"{chat_id}")
+            data.add_field("caption", text)
+            data.add_field("disable_notification", "false")
+            async with aiofiles.open(image_path, "rb") as photo:
+                photo_data = await photo.read()
+                data.add_field("photo", photo_data, filename=image_path.name)
 
-    else:
-        url = f"https://api.telegram.org/bot{settings.TG_BOT_TOKEN.get_secret_value()}/sendMessage"
-        data = {
-            "chat_id": f"{settings.TG_ID}",
-            "text": text,
-            "disable_notification": False,
-        }
+        else:
+            url = f"https://api.telegram.org/bot{settings.TG_BOT_TOKEN.get_secret_value()}/sendMessage"
+            data = {
+                "chat_id": f"{chat_id}",
+                "text": text,
+                "disable_notification": False,
+            }
 
-    async with aiohttp.ClientSession() as session:
-        await session.post(url, data=data, ssl=False)
+        async with ClientSession() as session:
+            await session.post(url, data=data, ssl=False)
 
 
 def send_report_at_exit():
-    url = (
-        f"https://api.telegram.org/bot{settings.TG_BOT_TOKEN.get_secret_value()}"
-        f"/sendMessage?text=Worker stopped&chat_id={settings.TG_ID}"
-    )
-    get(url)
+    for chat_id in settings.REPORT_TG_IDS:
+        url = (
+            f"https://api.telegram.org/bot{settings.TG_BOT_TOKEN.get_secret_value()}"
+            f"/sendMessage?text=Worker stopped&chat_id={chat_id}"
+        )
+        get(url)
