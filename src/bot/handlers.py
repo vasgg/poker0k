5f7cfab9c435c5e22ff51be690f9b9c95a6f31b7
vas g <vas.g@me.com>
2025-03-09 20:52:21 +0400

    wip

--- src/bot/handlers.py
+++ src/bot/handlers.py
@@ -2,14 +2,13 @@ from aiogram import F, Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
-from config import settings
 from controllers.actions import Actions
 from internal.consts import WorkspaceCoords
 
 router = Router()
 
 
-@router.message(Command("balance"), F.chat_id == settings.TG_REPORTS_CHAT)
+@router.message(Command("balance"), F.chat_id == -4655514577)
 async def balance(message: Message):
     picture = await Actions.take_screenshot_of_region(
         WorkspaceCoords.BALANCE_WINDOW_TOP_LEFT, WorkspaceCoords.BALANCE_WINDOW_BOTTOM_RIGHT
