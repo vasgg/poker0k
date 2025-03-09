da73597fc4389774542031c6440711171c0ad5af
vas g <vas.g@me.com>
2025-03-09 21:30:31 +0400

    wip

--- src/bot/handlers.py
+++ src/bot/handlers.py
@@ -1,5 +1,7 @@
+from io import BytesIO
+
 from aiogram import F, Router
-from aiogram.types import Message
+from aiogram.types import BufferedInputFile, Message
 
 from controllers.actions import Actions
 from internal.consts import WorkspaceCoords
@@ -12,4 +14,11 @@ async def balance(message: Message):
     picture = await Actions.take_screenshot_of_region(
         WorkspaceCoords.BALANCE_WINDOW_TOP_LEFT, WorkspaceCoords.BALANCE_WINDOW_BOTTOM_RIGHT
     )
-    await message.answer_photo(picture, disable_notification=True)
+    buffer = BytesIO()
+    picture.save(buffer, format='PNG')
+    buffer.seek(0)
+
+    await message.answer_photo(
+        BufferedInputFile(buffer.read(), filename='balance.png'),
+        disable_notification=True
+    )
