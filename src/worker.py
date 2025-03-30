3c585b255b7d48680dab6035350622a313d447be
vas g <vas.g@me.com>
2025-03-30 20:29:12 +0400

    wip timer

--- src/worker.py
+++ src/worker.py
@@ -15,7 +15,7 @@ last_restart_time: datetime | None = None
 async def check_time(mouse: Controller):
     global last_restart_time
     current_time = datetime.now(timezone(timedelta(hours=3)))
-    if (current_time - last_restart_time) >= timedelta(minutes=50):
+    if (current_time - last_restart_time) >= timedelta(minutes=35):
         logging.info("Performing scheduled restart app.")
         await Actions.reopen_pokerok_client(mouse)
         last_restart_time = current_time
