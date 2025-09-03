d8c763e9f98aeffa4241fbb9cb5e5374b29a24b7
vasgg <vas.g@me.com>
2025-09-03 06:29:00 +0400

    fix chat id config

--- src/bot/handlers.py
+++ src/bot/handlers.py
@@ -7,7 +7,7 @@ from controllers.telegram import get_balance_pic
 router = Router()
 
 
-@router.message(F.chat.id == settings.TG_REPORTS_CHAT, F.text == "/balance")
+@router.message(F.chat.id == int(settings.TG_REPORTS_CHAT), F.text == "/balance")
 async def balance(message: Message):
     buffer = await get_balance_pic()
     await message.answer_photo(BufferedInputFile(buffer.read(), filename="balance.png"), disable_notification=True)
