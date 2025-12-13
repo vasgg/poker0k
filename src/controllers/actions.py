3dace87c05f6b546f57d7946499dcd11a42617a2
Vasilii Gavrilov <vasilii.gavrilov@gcore.com>
2025-12-13 18:03:58 +0400

    new consts

--- src/controllers/actions.py
+++ src/controllers/actions.py
@@ -169,7 +169,7 @@ async def start_app_flow(mouse: Controller):
     await Actions.click_on_const(mouse, Coords.OPEN_APP_BUTTON)
     await Actions.click_on_const(mouse, Coords.OPEN_APP_BUTTON, 35)
     await Actions.click_on_const(mouse, Coords.LOGIN_BUTTON, 35)
-    await Actions.click_on_const(mouse, Coords.CONFIRM_LOGIN_BUTTON, 25)
+    await Actions.click_on_const(mouse, Coords.CONFIRM_LOGIN_BUTTON, 35)
     login_button = await Actions.find_square_color(
         color=Colors.WHITE,
         coordinates=(
